2ad5c9d4651a787f8c04c826ae168562f1adda7e
Josh Leeb-du Toit <mail@joshleeb.com>
2019-04-17 09:55:41 +1000

    Remove unimplemented MathjaxPolicy::Auto
    `MathjaxPolicy::Auto` will be implemented properly in the future when we have a
    better idea on performance profile.

--- src/render/mathjax.rs
+++ src/render/mathjax.rs
@@ -1,23 +1,19 @@
 use std::{io, str::FromStr};
 
+// TODO: render::mathjax implement `Auto` policy
 #[derive(Debug, PartialEq)]
 pub(crate) enum MathjaxPolicy {
-    Auto,
     Always,
     Never,
 }
 
 impl MathjaxPolicy {
     pub(crate) fn variants() -> &'static [&'static str] {
-        &["auto", "always", "never"]
+        &["always", "never"]
     }
 
-    // TODO: MathjaxPolicy::should_include smarter inclusion check for auto
     pub(crate) fn should_include(&self) -> bool {
-        match self {
-            MathjaxPolicy::Auto | MathjaxPolicy::Always => true,
-            _ => false,
-        }
+        *self == MathjaxPolicy::Always
     }
 }
 
@@ -26,7 +22,6 @@ impl FromStr for MathjaxPolicy {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
-            "auto" => Ok(MathjaxPolicy::Auto),
             "always" => Ok(MathjaxPolicy::Always),
             "never" => Ok(MathjaxPolicy::Never),
             s => Err(io::Error::new(
